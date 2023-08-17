b73e7c2a45967628e8a330a9f1ccb75061410d1b
sergio-g-v <67209713+cattus09@users.noreply.github.com>
2023-08-17 12:57:45 -0500

    Add files via upload

---
+++ arep/src/main/java/arep/browser.java
@@ -0,0 +1,40 @@
+package arep;
+
+public class browser {
+    String url;
+    public browser(String url){
+        this.url = url;
+    }
+
+    public void getProtocol(){
+
+    }
+
+    public void getAuthority(){
+
+    }
+
+    public void getHost(){
+
+    }
+
+    public void getPort(){
+
+    }
+
+    public void getPath(){
+
+    }
+
+    public void getQuery(){
+
+    }
+
+    public void getFile(){
+
+    }
+
+    public void getRef(){
+
+    }
+}
